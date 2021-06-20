691e59cd2cd5d849df024f4baacc1b82cfd222e6
Saffat Aziz <saziz028@uottawa.ca>
2021-06-19 21:05:01 -0400

    uploaded starter files

---
+++ giden-interview/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Giden Interview</h1>
+      // https://randomuser.me/api/?results=20
+    </div>
+  );
+}
+
+export default App;
